22dd706e4af7ed2f1889576c1f6f1be03e4d0e16
Ilya <brineone@gmail.com>
2019-03-21 22:29:18 +0400

    Мини псы становятся умнее!

--- GAME/Assets/AngryCube_test.cs
+++ GAME/Assets/AngryCube_test.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class AngryCube_test : MonoBehaviour
 {
-    public float distantion;
     private float nextActionTime = 0.5f;
     public GameObject color;
-    public float floatrange = 50.0f;
+    public GameObject Bot;
+    public float floatrange = 20.0f;
     public float timeToNextStep = 0.005f;
-
+    public float bot_skill_cast_range = 10f;
+    public float bot_skill_cd = 3f;
+    public float bot_skill_Delay = 0f;
+    public float bot_skill_cast_delay = 3f;
+    public Player player;
     void Start()
     {
         color = GameObject.FindGameObjectWithTag("Player");
+        player = color.GetComponent<Player>();
     }
 
     void Update()
     {
-
+        bot_skill_Delay -= Time.deltaTime;
         if ((Time.time > nextActionTime) && (Vector3.Distance(color.transform.position, transform.position) > floatrange))
         {
             float rx = Random.Range(-4f, 4f);
@@ -26,6 +31,37 @@ public class AngryCube_test : MonoBehaviour
             nextActionTime += timeToNextStep;
         }
         else
+        {
             transform.position = Vector3.MoveTowards(transform.position, color.transform.position, Time.deltaTime);
+            if (Vector3.Distance(color.transform.position, transform.position) < bot_skill_cast_range && bot_skill_Delay <= 0)
+            {
+                bot_skill_cast_delay -= Time.deltaTime;
+                if (bot_skill_cast_delay >= 2 && bot_skill_cast_delay < 3)
+                    Bot.GetComponent<Renderer>().material.color = Color.green;
+                else if (bot_skill_cast_delay >= 1 && bot_skill_cast_delay < 2)
+                    Bot.GetComponent<Renderer>().material.color = Color.yellow;
+                else if (bot_skill_cast_delay > 0 && bot_skill_cast_delay < 1)
+                    Bot.GetComponent<Renderer>().material.color = Color.magenta;                   
+                if (bot_skill_cast_delay <= 0)
+                {
+                    Bot.GetComponent<Renderer>().material.color = Color.black;
+                    player.hp--;
+                    RefreshBotDelay();
+                    Bot.GetComponent<Renderer>().material.color = Color.red;
+                    bot_skill_cast_delay = 3f;
+                }
+            }
+            else if (Vector3.Distance(color.transform.position, transform.position) > bot_skill_cast_range)
+            {
+                bot_skill_cast_delay = 3f;
+                RefreshBotDelay();
+                Bot.GetComponent<Renderer>().material.color = Color.red;
+            }
+
+        }
+    }
+    void RefreshBotDelay()
+    {
+        bot_skill_Delay = bot_skill_cd;
     }
 }
