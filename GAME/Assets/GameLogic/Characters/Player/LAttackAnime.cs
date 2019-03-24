12138d6bd8b75986f732cac341ff505be4bfbdba
Daniil Ostroukhov <ostr.dan@yandex.ru>
2019-03-24 19:33:05 +0400

    Anime na ave

---
+++ GAME/Assets/GameLogic/Characters/Player/LAttackAnime.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LAttackAnime : MonoBehaviour
+{
+    public Transform Arm;
+    public AnimationClip Attack_1;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Arm.GetComponent<Animator>().Play("ArmAttack");
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            Arm.GetComponent<Animator>().Play("New State");
+        }
+    }
+}
