10d7518561562884e68b54dc4cf4c750316db166
Ridersar <ak.arty@mail.ru>
2019-04-02 21:12:20 +0400

    Метеоритный дождь

---
+++ GAME/Assets/GameLogic/World/Objects/Meteor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.red; //метеорит "горячий"
+    }
+
+    void OnCollisionEnter(Collision collision) //столкновение
+    {
+        if ((gameObject.GetComponent<Renderer>().material.color == Color.red) && (collision.collider.tag == "Player")) //если метеорит "горячий" и соприкасается с игроком
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Destr>().TakeDamage(1); //нанесение урона игроку
+        if ((gameObject.GetComponent<Renderer>().material.color == Color.red) && (collision.collider.tag == "enemy")) //если метеорит "горячий" и соприкасается с мобом
+            Destroy(GameObject.FindGameObjectWithTag("enemy")); //уничтожение мобов
+        GetComponent<AudioSource>().Play(); //звук удара о землю
+        Invoke("Cooling", 10); //остывание
+        Invoke("Delete", 20); //исчезновение
+    }
+
+    void Cooling() //остывание
+    {
+        GetComponent<Renderer>().material.color = Color.green; //смена цвета при остывании
+    }
+
+    void Delete() //исчезновение
+    {
+        Destroy(gameObject); //уничтожение объекта
+    }
+
+}
---
+++ GAME/Assets/GameLogic/World/Objects/SpawnMeteors.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMeteors : MonoBehaviour
+{
+    public GameObject target;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            GetComponent<AudioSource>().Play();
+            for (int i = 0; i < 55; i++)
+                Invoke("Spawn", i);
+        }
+    }
+
+    void Spawn()
+    {
+        float x = Random.Range(-100.0f, 100.0f); //координата спавна
+        float y = Random.Range(200.0f, 300.0f); //координата спавна
+        float z = Random.Range(-100.0f, 100.0f); //координата спавна
+        Instantiate(target, new Vector3(x, y, z), Quaternion.identity); //спавн
+    }
+}
